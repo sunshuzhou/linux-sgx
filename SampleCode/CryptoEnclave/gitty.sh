c2cff95c5993338b6348fbb612dbe22c03e79ecc
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2016-12-10 17:29:46 -0500

    SampleCode/CryptoEnclave/gitty.sh:

--- SampleCode/CryptoEnclave/gitty.sh
+++ SampleCode/CryptoEnclave/gitty.sh
@@ -6,8 +6,11 @@
 
 # To setup under a Linux box after git clone REPO_ADDRESS
 # git config --global user.email EMAIL
+# git config --global user.name NAME
 # git config --system user.email EMAIL
+# git config --system user.name NAME
 # git config --local  user.email EMAIL
+# git config --local user.name NAME
 
 if [ -z "$1" ] || [ $# -gt 2 ]; then
     echo "Usage: ./gitty.sh FILE [\"COMMENT\"]";
